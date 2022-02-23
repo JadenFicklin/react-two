ce27415d471d2e5db0537319c025855df41f1d5d
JadenFicklin <jadendficklin@gmail.com>
2022-02-23 09:26:17 -0700

    codewars challenge

--- 22/index.js
+++ 22/index.js
@@ -1,1 +1,12 @@
-console.log("test");
+//Given a non-empty array of integers, return the result of multiplying the values together in order.
+
+const grow = (x) => {
+  let res = 1;
+  for (let i = 0; i < x.length; i++) {
+    res *= x[i];
+  }
+  return res;
+};
+
+let arr1 = [1, 2, 3, 4];
+console.log(grow(arr1));
